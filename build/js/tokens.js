bc0206ddcc8f8da29a1e2b96ca9f525273d39197
RicoPessoa <ricardoandre.pessoa@gmail.com>
2023-01-25 21:32:06 -0300

    first commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 26 Jan 2023 00:03:21 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray400": "#a1a1aa",
+  "ColorGray700": "#3f3f46",
+  "ColorGray800": "#27272a",
+  "ColorGray900": "#18181b",
+  "ColorPurple500": "#8b5cf6"
+};
